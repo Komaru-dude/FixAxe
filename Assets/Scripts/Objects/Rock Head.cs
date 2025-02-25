674afc016d3c9fc6cfb36e04fa04e915a8682a80
Komaru-dude <honkhonkovic842@gmail.com>
2025-02-26 00:46:29 +0700

    Продолжение работы с ловушкой

--- Assets/Scripts/Objects/Rock Head.cs
+++ Assets/Scripts/Objects/Rock Head.cs
@@ -1,2 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+public class RH : MonoBehaviour
+{
+    [SerializeField] private Animator animator;  // Ссылка на Animator
+    [SerializeField] private float acceleration = 1f;  // Ускорение
+    [SerializeField] private float maxSpeed = 10f;     // Максимальная скорость
+    [SerializeField] private LayerMask wallLayer;      // Слой стены
+    [SerializeField] private List<RockHeadAnims> selectedActions = new List<RockHeadAnims>();  // Список выбранных анимаций
+    [SerializeField] private RotationDirection rotationDirection;  // Направление движения (из Enum)
+
+    private float currentSpeed = 0f;  // Текущая скорость
+
+    void Update()
+    {
+
+    }
+}
