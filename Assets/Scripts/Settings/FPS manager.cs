74da084fc76f557c14998091b3c55ab7645fd3e6
Komaru-dude <honkhonkovic842@gmail.com>
2025-08-29 00:31:26 +0700

    0.24c

--- Assets/Scripts/Settings/FPS manager.cs
+++ Assets/Scripts/Settings/FPS manager.cs
@@ -10,19 +10,35 @@ public class FPSManager : MonoBehaviour
     public static void ApplyFpsSettings()
     {
         // Получаем частоту экрана в Гц
-        int maxRefreshRate = Mathf.RoundToInt((float)Screen.currentResolution.refreshRateRatio.value);
+        int screenFps = Mathf.RoundToInt((float)Screen.currentResolution.refreshRateRatio.value);
+        int maxFps = screenFps;
+
+#if UNITY_ANDROID
+        int androidFps = 0;
+        try
+        {
+            using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            using (var windowManager = activity.Call<AndroidJavaObject>("getSystemService", "window"))
+            using (var display = windowManager.Call<AndroidJavaObject>("getDefaultDisplay"))
+            using (var mode = display.Call<AndroidJavaObject>("getMode"))
+            {
+                androidFps = Mathf.RoundToInt(mode.Call<float>("getRefreshRate"));
+            }
+        }
+        catch
+        {
+            androidFps = 0;
+        }
+
+        maxFps = Mathf.Max(screenFps, androidFps);
+#endif
 
         // Читаем настройку
         bool highFpsEnabled = PlayerPrefs.GetInt("HighFpsEnabled", 0) == 1;
+        // Ставим поддерживаемый максимум
+        Application.targetFrameRate = highFpsEnabled ? maxFps : 60;
 
-        if (highFpsEnabled)
-        {
-            // Ставим максимум, который поддерживает экран
-            Application.targetFrameRate = maxRefreshRate;
-        }
-        else
-        {
-            Application.targetFrameRate = 60;
-        }
+        Debug.Log($"FPSManager: screen={screenFps}, target={Application.targetFrameRate}");
     }
 }
